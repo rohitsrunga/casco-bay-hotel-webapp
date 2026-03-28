4aff1488929040793c4391046cc909f0dd372268
Rohit S <rohit@Mac-610.lan>
2026-03-28 00:16:00 -0400

    add https to site

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import cloudflare from '@astrojs/cloudflare';
 // https://astro.build/config
 export default defineConfig({
   adapter: cloudflare(),
-  site: 'cascobayhotel.com',
+  site: 'https://cascobayhotel.com',
   output: 'static',
 });
